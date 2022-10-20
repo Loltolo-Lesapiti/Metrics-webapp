6e09efb44bb40a6624798cf42d2d078b1cb285ad
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-10-20 16:07:55 +0300

    Create and style a slider

---
+++ src/components/Home/slider.js
@@ -0,0 +1,49 @@
+import Carousel from 'react-bootstrap/Carousel';
+import slide1 from '../../images/dollar.png'
+import slide2 from '../../images/currency.jpg'
+import slide3 from '../../images/money.jpg'
+import "./slider.css";
+
+function Slider() {
+  return (
+    <Carousel className='slider'>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src={slide1}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src={slide2}
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={slide3}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Slider;
