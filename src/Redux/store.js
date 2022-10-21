cdc91344523a41b9f400baaad4275f5fb2b42600
Petro Lesapiti <petrolesapiti@gmail.com>
2022-10-21 10:38:11 +0300

    Update the configure store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rocketSlice from "./forexslice";
-import  exchangeSlice from "./currencyslice";
 
 const store = configureStore({
   reducer: {
     currency: rocketSlice,
-    exchange:exchangeSlice,
   },
 });
 
