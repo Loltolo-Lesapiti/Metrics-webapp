7fdb23445d93daec4842146c5f3a270560e7311d
Petro Lesapiti <petrolesapiti@gmail.com>
2022-10-22 04:18:32 +0300

    Update index js file to solve merge conflicts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -8,7 +7,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
