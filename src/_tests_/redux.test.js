4afa3e49ac1b0382d60621873164dd6e043dd6b9
Petro Lesapiti <petrolesapiti@gmail.com>
2022-10-22 03:15:53 +0300

    Mock configure store and the reducers

---
+++ src/_tests_/redux.test.js
@@ -0,0 +1,9 @@
+import store from "./mock/store";
+import { fetchCurrencies } from "./mock/forexslice";
+
+describe("test the store", () => {
+  it("Check if there is currencies in the store after fetching data", async () => {
+    await store.dispatch(fetchCurrencies());
+    expect(store.getState().currency.currency.ticker).toEqual("USD/JPY");
+  });
+});
