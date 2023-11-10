d0aa8a16d7a14c75a2be358cbec0972773358a66
Petro Lesapiti <petrolesapiti@gmail.com>
2023-11-10 08:48:31 +0300

    Update header

--- src/components/DetailsPage/index.js
+++ src/components/DetailsPage/index.js
@@ -1,5 +1,13 @@
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import {
+  List,
+  ListItem,
+  ListItemSuffix,
+  Chip,
+  Card,
+  Typography,
+} from "@material-tailwind/react";
 
 const CurrencyExchange = () => {
   const currencies = useSelector((state) => state.currency.currency);
@@ -14,30 +22,79 @@ const CurrencyExchange = () => {
           }
         })
         .map((data) => (
-          <div>
-            <h4>Currency Information</h4>
-
-            <h5>Currency</h5>
-            <h5>{data.ticker}</h5>
-
-            <h5>Bid</h5>
-            <h5>{data.bid}</h5>
-
-            <h5>Ask</h5>
-            <h5>{data.ask}</h5>
-
-            <h5>Open</h5>
-            <h5>{data.open}</h5>
-
-            <h5>Low</h5>
-            <h5>{data.low}</h5>
-            <h5>High</h5>
-            <h5>{data.high}</h5>
-            <h5>Changes</h5>
-            <h5>{data.changes}</h5>
-            <h5>Date</h5>
-            <h5>{data.date}</h5>
-          </div>
+          <Card className="w-full mx-auto my-auto">
+            <Typography variant="h4" color="blue-gray">
+              Information on {data.ticker} currency excahnge
+            </Typography>
+            <List>
+              <ListItem>
+                Bid
+                <ListItemSuffix>
+                  <Chip
+                    value={data.ask}
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-full"
+                  />
+                </ListItemSuffix>
+              </ListItem>
+              <ListItem>
+                Open
+                <ListItemSuffix>
+                  <Chip
+                    value={data.open}
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-full"
+                  />
+                </ListItemSuffix>
+              </ListItem>
+              <ListItem>
+                Low
+                <ListItemSuffix>
+                  <Chip
+                    value={data.low}
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-full"
+                  />
+                </ListItemSuffix>
+              </ListItem>
+              <ListItem>
+                High
+                <ListItemSuffix>
+                  <Chip
+                    value={data.high}
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-full"
+                  />
+                </ListItemSuffix>
+              </ListItem>
+              <ListItem>
+                Changes
+                <ListItemSuffix>
+                  <Chip
+                    value={data.changes}
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-full"
+                  />
+                </ListItemSuffix>
+              </ListItem>
+              <ListItem>
+                Date
+                <ListItemSuffix>
+                  <Chip
+                    value={data.date}
+                    variant="ghost"
+                    size="sm"
+                    className="rounded-full"
+                  />
+                </ListItemSuffix>
+              </ListItem>
+            </List>
+          </Card>
         ))}
     </>
   );
